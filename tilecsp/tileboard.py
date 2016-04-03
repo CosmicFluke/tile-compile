5c25500cb65458d34fb2d1c5b259805239ea9eca
gekath <kathyge@gmail.com>
2016-04-03 18:41:34 -0400

    Created subclasses for Tile

--- tilecsp/tileboard.py
+++ tilecsp/tileboard.py
@@ -7,7 +7,7 @@ class TileBoard(CSP):
 
         tiles:          list of Tiles, (initial domain for each variable in the board)
         dimensions:     tuple of (int, int)
-        tile_positions  list of Variables, n x n sized array (n = dimensions)
+        vars:           list of Variables, n x n sized array (n = dimensions)
 
 
         (Optional)
@@ -41,9 +41,113 @@ class TileBoard(CSP):
 
 class Tile:
 
-    def __init__(self, id, orientation):
+    def __init__(self, id):
         self.id = id
-        self.orientation = orientation
+
+
+class TTile(Tile):
+
+    EDGES = (1, 2, 3, 4)
+    ORIENTATIONS = ("n", "e", "s", "w")
+    _EDGE_VALUES = (False, True, True, True), \
+                   (True, False, True, True), \
+                   (True, True, False, True), \
+                   (True, True, True, False)
+
+    MAPPINGS = dict(
+        zip(
+            EDGES,
+            map(
+                lambda tup: dict(zip(TTile.ORIENTATIONS, tup)),
+                _EDGE_VALUES)
+        )
+    )
+
+    def __init__(self, id, orientation):
+        Tile.__init__(id)
+        self.edges = TTile.MAPPINGS[orientation]
+
+
+class CrossTile(Tile):
+
+    EDGES = (1)
+    ORIENTATIONS = ("n", "e", "s", "w")
+    _EDGE_VALUES = (True, True, True, True)
+
+    MAPPINGS = dict(
+        zip(
+            EDGES,
+            map(
+                lambda tup: dict(zip(TTile.ORIENTATIONS, tup)),
+                _EDGE_VALUES)
+        )
+    )
+
+    def __init__(self, id, orientation):
+        Tile.__init__(id)
+        self.edges = CrossTile.MAPPINGS[orientation]
+
+class CornerTile(Tile):
+    EDGES = (1, 2, 3, 4)
+    ORIENTATIONS = ("n", "e", "s", "w")
+    _EDGE_VALUES = (True, True, False, False), \
+                   (False, True, True, False), \
+                   (False, False, True, True), \
+                   (True, False, False, True)
+    MAPPINGS = dict(
+        zip(
+            EDGES,
+            map(
+                lambda tup: dict(zip(TTile.ORIENTATIONS, tup)),
+                _EDGE_VALUES)
+        )
+    )
+
+    def __init__(self, id, orientation):
+        Tile.__init__(id)
+        self.edges = CornerTile.MAPPINGS[orientation]
+
+
+class LineTile(Tile):
+    EDGES = (1, 2)
+    ORIENTATIONS = ("n", "e", "s", "w")
+    _EDGE_VALUES = (False, True, False, True), \
+                   (True, False, True, False)
+    MAPPINGS = dict(
+        zip(
+            EDGES,
+            map(
+                lambda tup: dict(zip(TTile.ORIENTATIONS, tup)),
+                _EDGE_VALUES)
+        )
+    )
+
+    def __init__(self, id, orientation):
+        Tile.__init__(id)
+        self.edges = LineTile.MAPPINGS[orientation]
+
+
+def create_tiles(num_tiles):
+    '''
+    IN:
+        num_tiles: dictionary { Subclass of Tile : number of said tiles}
+
+    OUT:
+        list of Tiles
+    '''
+    tiles = []
+
+    count = 0
+    for tile in num_tiles.keys():
+        for i in range(num_tiles[tile]):
+            id = 'id' + '-' + str(count)
+            for orientation in tile.ORIENTATIONS:
+
+                value = tile(id, orientation)
+                tiles.append(value)
+            count += 1
+
+    return tiles
 
 
 def create_board(dim, tiles):
@@ -56,4 +160,6 @@ def create_board(dim, tiles):
         n x n matrix, each element is a Variable with initial domain
         being the tiles array
     '''
+
     return [Variable('V' + str((i, j)), tiles) for i in range(dim) for j in range(dim)]
+
